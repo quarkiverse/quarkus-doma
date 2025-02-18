3f5da36de80e0322af73d7a5b18e18f3c7b52eea
orekyuu <orekyuu@gmail.com>
2025-02-18 21:20:08 +0900

    Reformat

--- deployment/src/test/java/io/quarkiverse/doma/deployment/InjectConfigTest.java
+++ deployment/src/test/java/io/quarkiverse/doma/deployment/InjectConfigTest.java
@@ -1,6 +1,9 @@
 package io.quarkiverse.doma.deployment;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import jakarta.inject.Inject;
 import jakarta.transaction.Status;
